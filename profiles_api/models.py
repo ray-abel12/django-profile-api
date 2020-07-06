025ac1f2b4bcc040641102b0f9772bc1fd69be24
ray-abel12 <rayabel12@outlook.com>
2020-07-06 03:11:42 +0100

    update

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -15,6 +15,18 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
+        def create_super_user(self,email,name,password):
+            """Create a new super user with given details"""
+            user=self.create_user(email,name,password)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save(using=self._db)
+            
+            return user 
+
+
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database model for users in the system """
