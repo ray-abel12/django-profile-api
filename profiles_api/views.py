5f77872b8e18bfffa471239600e5f0d1ea9576d6
ray-abel12 <rayabel12@outlook.com>
2020-07-08 22:37:28 +0100

    update

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -14,11 +14,15 @@ class HelloApiView(APIView):
                       'Is similar to a traditional django view']
 
         return Response({'message': 'hello', 'an_apiview': an_apiview})
-    
-    def post(self,request):
+
+    def post(self, request):
         """ Create a hello message with our name """
         serializers = self.serializers_class(data=request.data)
 
         if serializers.is_valid():
             name = serializers.validated_data.get('name')
-        return serializers
+            message = f'Hello {name}'
+        return Response({'message': message})
+        else:
+            return Response(serializers.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
