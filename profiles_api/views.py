d155f68e01e6f0167162b4095d382da336082b67
ray-abel12 <rayabel12@outlook.com>
2020-07-08 22:22:25 +0100

    update

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,11 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 
+
 class HelloApiView(APIView):
     """Test the Api view"""
+    serializers_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Return a list api view features"""
         an_apiview = ['Uses Http Method as function(get,post,put,patch,delete)',
-        'Is similar to a traditional django view']
+                      'Is similar to a traditional django view']
 
-        return Response({'message':'hello','an_apiview':an_apiview})
+        return Response({'message': 'hello', 'an_apiview': an_apiview})
